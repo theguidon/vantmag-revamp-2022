5a26bfe069243b94f22a0c4b8f0ccb61514a2f63
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-04 22:19:53 +0800

    Finished overall structure of the footer. Lacks the vantmag logo and fonts from the figma file.

--- footer.php
+++ footer.php
@@ -3,25 +3,29 @@ FOOTER
     <div class="left-flex">
       <!-- vantmag logo -->
       <h4>Vantage Magazine</h4>
-      <p>(c) THE GUIDON 2023. All rights reserved.</p>
+      <p>© THE GUIDON 2023. All rights reserved.</p>
+      <br><br>
       <p>The Guidon is the official publication of Ateneo de Manila University.</p>
     </div>
     <div class="right-flex">
-      <b><p>CATEGORIES</p></b>
-      <ul class="right-container">
-        <div class="left-flex">
+      <div class="category-title">
+        <b><p>CATEGORIES</p></b>
+      </div>
+      <div class="category-items">
+        <ul class="ul-left">
           <li>TV & FILM</li>
           <li>FOOD</li>
           <li>THEATRE & ARTS</li>
           <li>VANTAGE</li>
-        </div>
-        <div class="right-flex">
+        </ul>
+        <ul class="ul-right">
           <li>MUSIC</li>
           <li>HYPE</li>
           <li>HUB</li>
           <li>ABOUT</li>
-        </div>
-      </ul>
+        </ul>
+      </div>
+      
     </div>
   </div>
   </body>
