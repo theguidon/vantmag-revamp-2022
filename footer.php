ac471b7b9edc35b75a2bb3b460eda32a79657a43
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-05 23:00:41 +0800

    Addressed Emman's comments. Re: correct elements, font usage and responsiveness.

--- footer.php
+++ footer.php
@@ -1,38 +1,40 @@
 FOOTER
-  <div class="container">
-    <div class="left-flex">
-      <div class="footer-title">
-        <div class="footer-title-left">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/EmblemWhite.png" alt="VantMag Logo"/>
+  <footer>
+    <div class="footer-container">
+      <div class="left-flex">
+        <div class="footer-title">
+          <div class="footer-title-left">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/EmblemWhite.png" alt="VantMag Logo"/>
+          </div>
+          <div class="footer-title-right">
+            <h3>Vantage Magazine</h3>
+          </div>
         </div>
-        <div class="footer-title-right">
-          <h3>Vantage Magazine</h3>
+        <p>© THE GUIDON 2023. All rights reserved.</p>
+        <br><br>
+        <p>The Guidon is the official publication of Ateneo de Manila University.</p>
+      </div>
+      <div class="right-flex">
+        <div class="category-title">
+          <b><p>CATEGORIES</p></b>
         </div>
+        <div class="category-items">
+          <ul class="ul-left">
+            <li>TV & FILM</li>
+            <li>FOOD</li>
+            <li>THEATRE & ARTS</li>
+            <li>VANTAGE</li>
+          </ul>
+          <ul class="ul-right">
+            <li>MUSIC</li>
+            <li>HYPE</li>
+            <li>HUB</li>
+            <li>ABOUT</li>
+          </ul>
+        </div>
+        
       </div>
-      <p>© THE GUIDON 2023. All rights reserved.</p>
-      <br><br>
-      <p>The Guidon is the official publication of Ateneo de Manila University.</p>
     </div>
-    <div class="right-flex">
-      <div class="category-title">
-        <b><p>CATEGORIES</p></b>
-      </div>
-      <div class="category-items">
-        <ul class="ul-left">
-          <li>TV & FILM</li>
-          <li>FOOD</li>
-          <li>THEATRE & ARTS</li>
-          <li>VANTAGE</li>
-        </ul>
-        <ul class="ul-right">
-          <li>MUSIC</li>
-          <li>HYPE</li>
-          <li>HUB</li>
-          <li>ABOUT</li>
-        </ul>
-      </div>
-      
-    </div>
-  </div>
+  </footer>
   </body>
 </html>
