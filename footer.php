15399fd68d72dcaca12b59c1b28b0b8e05948848
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-11 21:41:08 +0800

    Addressed comments: - change font to Poppins (or whatever is in Figma) -  fix font size

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-container">
-      <div class="left-flex bodytext-m">
+      <div class="left-flex -m">
         <div class="footer-title">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/LongFormWhite.png" alt="VantMag Logo"/>
         </div>
@@ -14,32 +14,32 @@
         </div>
 
         <div class="grid-container">
-          <div class="grid-item .bodytext">
-            <a href="#"></a><li>TV & FILM</li></a>
+          <div class="grid-item">
+            <a href="#"><li>TV & FILM</li></a>
           </div>
-          <div class="grid-item bodytext"> 
-            <a href="#"></a><li>FOOD</li></a>
+          <div class="grid-item"> 
+            <a href="#"><li>FOOD</li></a>
           </div>
-          <div class="grid-item bodytext">
-            <a \href="#"><li>THEATRE & ARTS</li></a>
+          <div class="grid-item">
+            <a href="#"><li>THEATRE & ARTS</li></a>
           </div>  
-          <div class="grid-item bodytext">
-            <a href="#"></a><li>VANTAGE</li></a>
+          <div class="grid-item">
+            <a href="#"><li>VANTAGE</li></a>
           </div>
-          <div class="grid-item bodytext">
-            <a href="#"></a><li>MUSIC</li></a>
+          <div class="grid-item">
+            <a href="#"><li>MUSIC</li></a>
           </div>
-          <div class="grid-item bodytext">
-            <a class="item" href="#"></a><li>EXPOSE</li></a>
+          <div class="grid-item">
+            <a class="item" href="#"><li>EXPOSE</li></a>
           </div>  
-          <div class="grid-item bodytext">
-            <a class="item" href="#"></a><li>HUB</li></a>
+          <div class="grid-item">
+            <a class="item" href="#"><li>HUB</li></a>
           </div>
-          <div class="grid-item bodytext">
-            <a class="item" href="#"></a><li>ABOUT</li></a>
+          <div class="grid-item">
+            <a class="item" href="#"><li>ABOUT</li></a>
           </div>
-          <div class="grid-item bodytext">
-            <a class="item" href="#"></a><li>HYPE</li></a>
+          <div class="grid-item">
+            <a class="item" href="#"><li>HYPE</li></a>
           </div>  
         </div>
       </div>
