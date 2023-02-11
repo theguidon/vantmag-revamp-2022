1fc55d177667442537474d74f4591193ce8c7dfb
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-02-12 03:05:01 +0800

    add latest articles (wip), chip

---
+++ templates/chip.php
@@ -0,0 +1,19 @@
+<?php
+  $t = get_term($args['id']);
+  $rev_name = vant_get_categ_tag_name($t->name);
+
+  $has_icon = false;
+  if (in_array($t->slug, array('expose', 'food', 'hub', 'hype', 'music', 'theater-and-the-arts', 'tv-and-film', 'vantage-point')))
+    $has_icon = true;
+?>
+
+<a
+  class="chip"
+  style="background-color: <?php echo vant_get_color($rev_name) ?>"
+  href="<?php echo get_term_link($args['id']) ?>"
+>
+  <?php if ($has_icon) { ?>
+    <img src="<?php echo get_template_directory_uri() . "/assets/images/icons/chip/" . $t->slug . ".svg" ?>" />
+  <?php } ?>
+  <?php echo $rev_name; ?>
+</a>
