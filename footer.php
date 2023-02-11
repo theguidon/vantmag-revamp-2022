0c65da64a01e20d01462356edb306b98c90421d6
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-11 15:01:43 +0800

    Addressed comment: pls use the logo! download from project sheet > first sheet > branding kit Addressed comment:  update links (and margins) Addressed comment: suggest that for elements like these, don't use <br>; instead, just use two <p>s with margin in between. if di ganun on figma, just estimate ilang px hehe Addressed comment: change footer to follow this setup: on larger screens, don't make the footer expand in width but just keep it all centered

--- footer.php
+++ footer.php
@@ -1,36 +1,48 @@
 <footer>
     <div class="footer-container">
-      <div class="left-flex">
+      <div class="left-flex bodytext-m">
         <div class="footer-title">
-          <div class="footer-title-left">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/EmblemWhite.png" alt="VantMag Logo"/>
-          </div>
-          <div class="footer-title-right">
-            <h3>Vantage Magazine</h3>
-          </div>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/LongFormWhite.png" alt="VantMag Logo"/>
         </div>
         <p>© The GUIDON 2023. All rights reserved.</p>
-        <br><br>
         <p>The Guidon is the official publication of Ateneo de Manila University.</p>
       </div>
+      
       <div class="right-flex">
-        <div class="category-title">
-          <b><p>CATEGORIES</p></b>
+        <div class="div2">  
+          <b><p class="category-title">CATEGORIES</p></b>
         </div>
-        <div class="category-items">
-          <ul class="ul-left">
-            <li>TV & FILM</li>
-            <li>FOOD</li>
-            <li>THEATRE & ARTS</li>
-            <li>VANTAGE</li>
-          </ul>
-          <ul class="ul-right">
-            <li>MUSIC</li>
-            <li>HYPE</li>
-            <li>HUB</li>
-            <li>ABOUT</li>
-          </ul>
+
+        <div class="grid-container">
+          <div class="grid-item .bodytext">
+            <a href="#"></a><li>TV & FILM</li></a>
+          </div>
+          <div class="grid-item bodytext"> 
+            <a href="#"></a><li>FOOD</li></a>
+          </div>
+          <div class="grid-item bodytext">
+            <a \href="#"><li>THEATRE & ARTS</li></a>
+          </div>  
+          <div class="grid-item bodytext">
+            <a href="#"></a><li>VANTAGE</li></a>
+          </div>
+          <div class="grid-item bodytext">
+            <a href="#"></a><li>MUSIC</li></a>
+          </div>
+          <div class="grid-item bodytext">
+            <a class="item" href="#"></a><li>EXPOSE</li></a>
+          </div>  
+          <div class="grid-item bodytext">
+            <a class="item" href="#"></a><li>HUB</li></a>
+          </div>
+          <div class="grid-item bodytext">
+            <a class="item" href="#"></a><li>ABOUT</li></a>
+          </div>
+          <div class="grid-item bodytext">
+            <a class="item" href="#"></a><li>HYPE</li></a>
+          </div>  
         </div>
+      </div>
         
       </div>
     </div>
