56bdaa493292fc73aead185bac13822e9c419e6b
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-12 18:03:38 +0800

    Addressed comments: -  add social media icons Instagram logo is incomplete. Downloaded zip file of both parts of the IG logo.

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
 <footer>
     <div class="footer-container">
       
-      <div class="left-flex -m">
+      <div class="left-flex">
         <div class="footer-title">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/LongFormWhite.png" alt="VantMag Logo"/>
         </div>
         <p>© The GUIDON 2023. All rights reserved.</p>
         <p>The GUIDON is the official publication of Ateneo de Manila University.</p>
+        <div class="socialmedia-icons">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/FB_icon.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Twitter_icon.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/IG_icon.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Spotify_icon.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/YT_icon.svg" alt="VantMag Logo"/>
+        </div>
       </div>
       
       <div class="right-flex">
