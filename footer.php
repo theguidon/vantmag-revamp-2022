21b91244eefd694b759a494ff1cc581e0b8f4f13
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-12 23:02:50 +0800

    Addressed comment:  rename icons from Facebook_icon.svg to just facebook.svg

--- footer.php
+++ footer.php
@@ -8,11 +8,11 @@
         <p>© The GUIDON 2023. All rights reserved.</p>
         <p>The GUIDON is the official publication of Ateneo de Manila University.</p>
         <div class="socialmedia-icons">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/FB_icon.svg" alt="VantMag Logo"/>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Twitter_icon.svg" alt="VantMag Logo"/>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/IG_icon.svg" alt="VantMag Logo"/>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Spotify_icon.svg" alt="VantMag Logo"/>
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/YT_icon.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/spotify.svg" alt="VantMag Logo"/>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.svg" alt="VantMag Logo"/>
         </div>
       </div>
       
