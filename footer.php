c496c5cf744876edb7dd06b9129e0ba7d4f67041
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2023-02-13 01:16:37 +0800

    Addressed comments: - add hyperlinks:  vant logo -> href="<?php echo home_url(); ?>"  fb, twt, ig, spotify, yt

--- footer.php
+++ footer.php
@@ -3,16 +3,16 @@
       
       <div class="left-flex">
         <div class="footer-title">
-          <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/LongFormWhite.png" alt="VantMag Logo"/></a>
+          <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/LongFormWhite.png" alt="VantMag Logo"/></a>
         </div>
         <p>© The GUIDON 2023. All rights reserved.</p>
         <p>The GUIDON is the official publication of Ateneo de Manila University.</p>
         <div class="socialmedia-icons">
-          <a><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/facebook.svg" alt="VantMag Logo"/></a>
-          <a><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/twitter.svg" alt="VantMag Logo"/></a>
-          <a><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/instagram.svg" alt="VantMag Logo"/></a>
-          <a><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/spotify.svg" alt="VantMag Logo"/></a>
-          <a><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/youtube.svg" alt="VantMag Logo"/></a>
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/facebook.svg" alt="VantMag Logo"/></a>
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/twitter.svg" alt="VantMag Logo"/></a>
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/instagram.svg" alt="VantMag Logo"/></a>
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/spotify.svg" alt="VantMag Logo"/></a>
+          <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/youtube.svg" alt="VantMag Logo"/></a>
         </div>
       </div>
       
