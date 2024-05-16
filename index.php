8e6d4f1e05af40cea74a8f63efe77f632484ccf6
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-16 19:22:32 +0800

    fix indent

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 <main id="index">
   <?php
-    get_template_part('template-parts/index/latest');
-    get_template_part('template-parts/index/suggestions');
-    get_template_part('template-parts/index/all-content');
+  get_template_part('template-parts/index/latest');
+  get_template_part('template-parts/index/suggestions');
+  get_template_part('template-parts/index/all-content');
   ?>
 </main>
 
