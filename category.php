12eb1da09aea7590febe27544cc6ba44f9b8a6c8
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-18 15:32:31 +0800

    init for category

--- category.php
+++ category.php
@@ -46,7 +46,57 @@ if (in_array($categ->slug, array('expose', 'food', 'hub', 'hype', 'music', 'thea
   </p>
 </section>
 
-<main class="general-container">
+<main class="categ-container general-container">
+  <div class="latest-container">
+    <?php
+    $article_counter = 0;
+    while ($latest_query->have_posts()) {
+      $latest_query->the_post();
+      $article_counter++;
+    ?>
+      <a
+        href="<?php echo the_permalink() ?>"
+        class="article"
+        style="grid-area: l<?php echo $article_counter ?>"
+      >
+        <div class="thumbnail-container">
+          <img
+            class="thumbnail"
+            src="<?php
+              if (has_post_thumbnail($post->ID)) {
+                echo wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0];
+              } else {
+                echo get_template_directory_uri() . "/assets/images/vantmag_16x9.png";
+              }
+            ?>"
+            alt="<?php
+              if (has_post_thumbnail($post->ID)) {
+                echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+              }
+            ?>"
+          />
+        </div>
+
+        <div class="info">
+          <h4 class="title"><?php the_title() ?></h4>
+          <p class="excerpt"><?php echo get_the_excerpt() ?></p>
+          <p class="authors">By 
+            <?php
+              if (function_exists('coauthors_posts_links')) {
+                coauthors();
+              } else {
+                the_author();
+              }
+            ?>
+          </p>
+          <p class="date"><?php echo get_the_date() ?></p>
+        </div>
+      </a>
+
+    <?php
+    }
+    ?>
+  </div>
 </main>
 
 <?php
