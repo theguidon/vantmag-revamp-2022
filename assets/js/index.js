1caa5b8739d24fde73a01e83b87b11560ee2dc96
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-21 01:11:30 +0800

    finish index/all-content for desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,11 @@ function vant_index_on_tab_click(slug) {
   jQuery(function ($) {
     $("#index #all-content .tab-selection button").removeClass("active")
     $(`#index #all-content .tab-selection button#${slug}`).addClass("active")
+
+    $("#index #all-content .tab.articles-grid").removeClass("active")
+    $(`#index #all-content #${slug}-tab`).addClass("active")
+
+    $("#index #all-content .load-more").removeClass("active")
+    $(`#index #all-content #${slug}-load-more`).addClass("active")
   })
 }
--- template-parts/index/all-content.php
+++ template-parts/index/all-content.php
@@ -50,6 +50,38 @@
 
   <?php
   for ($i = 0; $i < count($categs); $i++) {
+    $categ_query = new WP_Query(array(
+      'category_name' => $categs[$i],
+      'posts_per_page' => 9,
+    ));
+  ?>
+    <div
+      id="<?php echo $categs[$i] ?>-tab"
+      class="tab articles-grid <?php if ($i == 0) { echo "active"; } ?>"
+    >
+      <?php
+      if ($categ_query->have_posts()) {
+        while ($categ_query->have_posts()) {
+          $categ_query->the_post();
+          get_template_part('templates/article-card', null, array(
+            'current_post' => $categ_query->post,
+          ));
+        }
+      }
+      ?>
+    </div>
+
+    <a
+      id="<?php echo $categs[$i] ?>-load-more"
+      class="load-more <?php if ($i == 0) { echo "active"; } ?>"
+      href="<?php
+        if ($i != 6)
+          echo get_term_link(get_term_by('slug', $categs[$i], 'category'));
+        else
+          echo get_term_link(get_term_by('slug', $categs[$i], 'post_tag'));
+      ?>"
+    >Show me more</a>
+  <?php
   }
   ?>
 </section>
