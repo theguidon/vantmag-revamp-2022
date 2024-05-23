0382c8026ebddf739667ae29b38eb8548e9635e3
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-24 03:29:04 +0800

    nav fixes

--- functions.php
+++ functions.php
@@ -149,6 +149,15 @@ if (function_exists('register_nav_menus')) {
   );
 }
 
+add_filter('nav_menu_css_class', 'vant_active_nav_class', 10 , 2);
+
+function vant_active_nav_class($classes, $item) {
+  if (in_array('current-menu-item', $classes)) {
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
+
 
 /**
  * Returns short excerpt
